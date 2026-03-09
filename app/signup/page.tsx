04f1b836f81896996745a729c50ecd8491b22803
birgit-vosar <birgit.vosar@gmail.com>
2026-03-09 22:30:47 +0200

    Signup page functionality with error handling and redirect

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,29 +1,47 @@
+'use client'
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const [error, setError] = useState('');
+const router= useRouter();
+
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
+}
+
+
 export default function Signup() {
     return (
-        <div className="bg-[#0b111e] flex flex-col items-center justify-center min-h-screen  ">
-            <div className="bg-[#0e1525] p-6 border border-[#182543] rounded-lg w-full max-w-md">
+        <div className='bg-[#0b111e] flex flex-col items-center justify-center min-h-screen  '>
+            <div className='bg-[#0e1525] p-6 border border-[#182543] rounded-lg w-full max-w-md'>
                 <div>
-                    <h2 className="mb-6 text-center font-semibold text-2xl/9 tracking-tight">🦆 WorkFlow Companion</h2>
+                    <h2 className='mb-6 text-center font-semibold text-2xl/9 tracking-tight'>🦆 WorkFlow Companion</h2>
                 </div>
                 <div>
-                    <form action="/api/auth/signup" method="POST" className="space-y-6">
+                    <form onSubmit={handleSubmit} className='space-y-6'>
+                        {error && (
+                            <div className="bg-red-500/10 border border-red-500 text-red-500 px-4 py-2 rounded">
+                                {error}
+                            </div>
+                        )}
                         <div>
                             <label>
                                 Email
                             </label>
-                            <div className="mt-2">
+                            <div className='mt-2'>
                                 <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
+                                    id='email'
+                                    name='email'
+                                    type='email'
                                     required
-                                    autoComplete="email"
-                                    className="bg-[#0b111e] w-full outline-1 -outline-offset-1 outline-[#182543] placeholder:text-muted-foreground
+                                    autoComplete='email'
+                                    className='bg-[#0b111e] w-full outline-1 -outline-offset-1 outline-[#182543] placeholder:text-muted-foreground
                                     px-3 py-1.5
                                     rounded-md 
                                     sm:text-sm/6
-                                    "
-                                    placeholder="you@example.com"
+                                    '
+                                    placeholder='you@example.com'
                                 />
                             </div>
                         </div>
@@ -31,24 +49,24 @@ export default function Signup() {
                             <label>
                                 Password
                             </label>
-                            <div className="mt-2">
+                            <div className='mt-2'>
                                 <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
+                                    id='password'
+                                    name='password'
+                                    type='password'
                                     required
-                                    autoComplete="current-password"
-                                    className="bg-[#0b111e] w-full outline-1 -outline-offset-1 outline-[#182543] placeholder:text-muted-foreground
+                                    autoComplete='current-password'
+                                    className='bg-[#0b111e] w-full outline-1 -outline-offset-1 outline-[#182543] placeholder:text-muted-foreground
                                     px-3 py-1.5
                                     rounded-md
                                     sm:text-sm/6
-                                    "
-                                    placeholder="••••••••"
+                                    '
+                                    placeholder='••••••••'
                                 />
                             </div>
                         </div>
                         <div>
-                            <button className="items-center justify-center rounded-md text-sm font-medium bg-blue-500 h-10 w-full">
+                            <button type='submit' className='items-center justify-center rounded-md text-sm font-medium bg-blue-500 h-10 w-full'>
                                 Sign up
                             </button>
                         </div>
