a2b7eb79f6504a253b32cab3e3fe8ad4a228e36e
birgit-vosar <birgit.vosar@gmail.com>
2026-04-16 14:31:15 +0300

    added nav and topbar components to all pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,40 @@
 'use client'
 
-export default function() {
+import { useTheme } from "../context/ThemeContext"
+import Nav from "../components/Nav"
+import TopBar from "../components/TopBar"
+
+export default function () {
+    const { toggleTheme, darkMode } = useTheme()
+
     return (
-        <p>Hello world!</p>
+        <div className="grid grid-cols-8 min-h-screen">
+            <Nav />
+            <div className="col-span-7 bg-stone-100 text-zinc-800 dark:bg-[#0b111e] dark:text-white h-full">
+                <div className="flex flex-col h-screen">
+                    {/* topbar */}
+                    <TopBar />
+                    {/* main */}
+                    <div className="grow">
+                        <div className="grid grid-cols-10 min-h-full">
+                            <div className="col-span-6 bg-grey-700 h-full">
+                                <div className="flex flex-col">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <div className="col-span-4 bg-grey-800 h-full">
+                                <div className="flex flex-col">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- app/pomodoro/page.tsx
+++ app/pomodoro/page.tsx
@@ -1,7 +1,24 @@
 'use client'
 
-export default function() {
+import { useTheme } from "../context/ThemeContext"
+import Nav from "../components/Nav"
+import TopBar from "../components/TopBar"
+
+export default function () {
+    const { toggleTheme, darkMode } = useTheme()
+
     return (
-        <p>Hello world!</p>
+        <div className="grid grid-cols-8 min-h-screen">
+            <Nav />
+            <div className="col-span-7 bg-stone-100 text-zinc-800 dark:bg-[#0b111e] dark:text-white h-full">
+                <div className="flex flex-col h-screen">
+                    <TopBar />
+                    {/* main */}
+                    <div>
+                        <p className="ml-4 mt-4 text-lg">This is pomodoro page.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
